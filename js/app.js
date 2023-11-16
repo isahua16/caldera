ec0f28bebabe3a33cc31cbd20d853d41c3fb7f3e
isahua16 <isael.huard@calderainteractive.com>
2023-11-15 22:35:23 -0700

    adding more content and mobile menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
+function open_nav() {
+  let nav = document.querySelector(`#mobile_nav`);
+  nav.style.padding = `20px 0px`;
+  nav.style.visibility = `visible`;
+  nav.style.opacity = `1`;
+  nav.style.height = `200px`;
+}
 
+let menu = document.querySelector(`#menu_icon`);
+menu.addEventListener(`click`, open_nav);
