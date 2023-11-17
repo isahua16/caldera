da364234a3d372c8b4b9f11015b02e38a02a2d26
isahua16 <isael.huard@calderainteractive.com>
2023-11-16 20:40:46 -0700

    change padding in menu

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 function open_nav() {
   let nav = document.querySelector(`#mobile_nav`);
-  nav.style.paddingBottom = `20px`;
+  nav.style.paddingBottom = `40px`;
   nav.style.visibility = `visible`;
   nav.style.opacity = `1`;
   nav.style.height = `200px`;
